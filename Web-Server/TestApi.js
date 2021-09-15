7b7860f48d3682c48c2e5e8d64caf9a35fc73865
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-09-15 14:54:34 +0900

    기능::REST API DEMO 이윤수
    TestApi.js : 임의로 배열을 생성한 후,
    local로 테스트 하였습니다. 정상작동 확인 하였고
    Component별로 API생성하면 될 것 같습니다.

--- Web-Server/TestApi.js
+++ Web-Server/TestApi.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -6,7 +8,28 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
 	// console.log(res.json(users));
-	return res.json(users);
+	res.send(users);
+});
+
+app.get('/users/:id', (req, res) => {
+	// /users/id값을 받으면 입력 받은 값을 반환.
+	console.log(req.params.id); //받은 id값을 확인
+	const id = parseInt(req.params.id, 10); //id 값을 변수로 선언
+	if (id == 1) {
+		//반복문으로 리팩토링 가능
+		//id가 ~~일 경우
+		res.send(users[0]);
+	} else if (id == 2) {
+		res.send(users[1]);
+	} else if (id == 3) {
+		res.send(users[2]);
+	} else if (id == 4) {
+		res.send(users[3]);
+	} else if (id == 5) {
+		res.send(users[4]);
+	} else {
+		res.send('Data를 찾을 수 없습니다.');
+	}
 });
 
 app.listen(3000, () => {
@@ -35,3 +58,16 @@ let users = [
 		name: 'yunsu55',
 	},
 ];
+
+/*var returnResult = function (err, res) {
+	// 결과를 눈으로 보기 쉽게하기 위해 result 객체 생성
+	var result = {};
+	if (err) {
+		res.status(400);
+		result.message = err.stack;
+	} else {
+		res.status(200);
+		result.message = 'Success';
+	}
+	return result;
+};*/
