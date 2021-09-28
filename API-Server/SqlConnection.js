441996b8e2427b8e8c551c8d6231617b7c8053ea
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-09-28 13:14:53 +0900

    기능::table생성문 추가 이윤수
    SqlConnection.js의 Playlist, PlaylistItem,
    Video 함수에 모두 Table 생성문을 추가했습니다.
    정리되지 않은 코드이며 검증이 필요합니다.
    DB연결 후, 테스트 부탁드리겠습니다.

--- API-Server/SqlConnection.js
+++ API-Server/SqlConnection.js
@@ -10,6 +10,19 @@ var exports = (module.exports = {});
 // 	console.log('가져온 정보입니다. : ' + response[0].snippet.title); //반복문으로 돌려두면 됩니다.
 // 	NoNameFunc(response);
 // });
+//기존 sqlconnection.js script
+var con = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'root',
+	password: '111111',
+	database: 'webdrama',
+	charset: 'utf8mb4',
+});
+
+con.connect(function (err) {
+	if (err) throw err;
+	console.log('Connected!');
+});
 
 NoNameFunc = (response, apicode) => {
 	//아직 이름을 못지어 주었습니다.. Playlist와 PlaylisItem의 정보가 50개 이상일 떄 동작하는 함수입니다.
@@ -37,6 +50,29 @@ PlayList = (token) => {
 	console.log('token입니다. ' + token);
 	Playlist.Data(token, function (response) {
 		console.log('가져온 정보입니다. : ' + response[0].snippet.title); //반복문으로 돌려두면 됩니다.
+
+		//Request data 등록
+		let datanum = 0;
+		while (datanum < response.length) {
+			let data = response[datanum].snippet;
+			var sql = 'INSERT INTO episode_video(title, description, channelId, likeCount) VALUES(?,?,?,?)'; //컬럼은 따로 변경 부탁드립니다.
+			var params = [];
+			params.push(data.title);
+			params.push(data.description);
+			params.push(data.publishedAt);
+			params.push(data.channelId);
+			params.push(data.channelTitle);
+			params.push(data.thumbnails.high.url);
+			console.log(params);
+
+			con.query(sql, params, function (err, rows, fields) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(rows);
+				}
+			});
+		}
 		//조건문으로 stop 해줍시다.
 		if (response.length == 50) {
 			//조회데이터가 50개 이상일 경우
@@ -49,6 +85,25 @@ PlayListItem = (token) => {
 	const apicode = 'playlistitem';
 	PlaylistItem.Data(token, function (response) {
 		console.log('가져온 정보입니다. : ' + response[2].snippet.resourceId.videoId); //반복문으로 돌려두면 됩니다.
+
+		//Request data 등록
+		let datanum = 0;
+		while (datanum < response.length) {
+			let data = response[datanum].snippet;
+			let sql = 'INSERT INTO episode_video(title, description, channelId, likeCount) VALUES(?,?,?,?)'; //컬럼은 따로 변경 부탁드립니다.
+			let params = [];
+			params.push(data.channelId);
+			params.push(data.resourceId.videoId);
+			console.log(params);
+
+			con.query(sql, params, function (err, rows, fields) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(rows);
+				}
+			});
+		}
 		if (response.length == 50) {
 			//조회데이터가 50개 이상일 경우
 			NoNameFunc(response, apicode);
@@ -60,42 +115,25 @@ Video = () => {
 	const apicode = 'video';
 	Video.Data(token, function (response) {
 		console.log('가져온 정보입니다.' + response);
+		let data = response.snippet;
+		let likecount = response.statistics;
+		let sql = 'INSERT INTO episode_video(title, description, channelId, likeCount) VALUES(?,?,?,?)';
+		let params = [];
+		params.push(data.title);
+		params.push(data.description);
+		params.push(data.channelId);
+		params.push(likecount.likeCount);
+		console.log(params);
+
+		con.query(sql, params, function (err, rows, fields) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(rows);
+			}
+		});
 	});
 };
 
 // PlayList(undefined);
 PlayListItem(undefined);
-
-//기존 sqlconnection.js script
-var con = mysql.createConnection({
-	host: '127.0.0.1',
-	user: 'root',
-	password: '111111',
-	database: 'webdrama',
-	charset: 'utf8mb4',
-});
-
-con.connect(function (err) {
-	if (err) throw err;
-	console.log('Connected!');
-});
-
-Video.Data(function (response) {
-	let data = response.snippet;
-	let likecount = response.statistics;
-	var sql = 'INSERT INTO episode_video(title, description, channelId, likeCount) VALUES(?,?,?,?)';
-	var params = [];
-	params.push(data.title);
-	params.push(data.description);
-	params.push(data.channelId);
-	params.push(likecount.likeCount);
-	console.log(params);
-
-	con.query(sql, params, function (err, rows, fields) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(rows);
-		}
-	});
-});
