374d9a4112ac4946e1b3e9d0f6d08e64e12627d8
DEENNO <yuntteng31@gmail.com>
2021-12-15 22:24:07 +0900

    테스트:: python 인수전달 기능 추가이윤수
    routes/src/search.py : 인수전달 모듈을 추가하고 기능을 추가하였습니다.
    검색기능 만들어주시면 됩니다.

--- routes/pytest2.js
+++ routes/pytest2.js
@@ -5,13 +5,13 @@ const path = require('path');
 const searchScript = path.resolve('routes', 'src', 'search.py');
 console.log(`파일경로입니다 : ${searchScript}`);
 app.get('/', (req, res) => {
-	const {spawn} = require('child_process');
-	const search = spawn('python3', [searchScript]);
+  const {spawn} = require('child_process');
+  const search = spawn('python3', [searchScript, '이윤수', '20']);
 
-	search.stdout.on('data', function (data) {
-		console.log('파이썬 파일에서 반환받은 성분입니다. : ' + data.toString());
-		res.json(data.toString());
-	});
+  search.stdout.on('data', (data) => {
+    console.log('파이썬 파일에서 반환받은 성분입니다. : ' + data.toString());
+    res.json(data.toString());
+  });
 });
 
 app.listen(4000, () => console.log('Application listening on port 4000!'));
--- routes/src/search.py
+++ routes/src/search.py
@@ -1,13 +1,11 @@
-# function_args.py 파일 
-#import sys 
+# function_args.py 파일
+print('Hello, python3!!!\n')
+
+# function_args.py 파일
+import sys
+def getName(name, age):
+    print (name + " : " + age)
+if __name__ == '__main__':
+    getName(sys.argv[1], sys.argv[2])
 
 
-
-print('Hello, python3!!!')
-
-
-# def getName(name, age):
-#    print (name + " : " + age)
-
-# if __name__ == '__main__':
-#    getName(sys.argv[1], sys.argv[2])
